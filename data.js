71a6dadc3c2e5c7115dc387d6a38be2177a1b7c8
JEROLDINA <jerol5300@gmail.com>
2025-03-08 16:55:21 +0200

    Upload

---
+++ data.js
@@ -0,0 +1,187 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-render1",
+      "name": "render1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.027946717591710524,
+          "pitch": 0.2117747992038641,
+          "rotation": 6.283185307179586,
+          "target": "3-render4"
+        },
+        {
+          "yaw": -1.6317395732095985,
+          "pitch": 0.204381482165779,
+          "rotation": 0,
+          "target": "1-render2"
+        },
+        {
+          "yaw": 3.0795064301744803,
+          "pitch": 0.3042325716748042,
+          "rotation": 0,
+          "target": "2-render3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-render2",
+      "name": "render2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.7425662163431319,
+        "pitch": -0.0012929799166307987,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5466573682283542,
+          "pitch": 0.21996492919756783,
+          "rotation": 0,
+          "target": "0-render1"
+        },
+        {
+          "yaw": 0.44746186241835595,
+          "pitch": 0.13851980345632953,
+          "rotation": 7.0685834705770345,
+          "target": "3-render4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-render3",
+      "name": "render3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -3.0824402953944396,
+        "pitch": -0.08058137385680375,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.012596057392393334,
+          "pitch": 0.20340282565489076,
+          "rotation": 0,
+          "target": "0-render1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-render4",
+      "name": "render4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.24665891839313403,
+        "pitch": 0.04225218140778608,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0823112786384677,
+          "pitch": 0.3370195290525313,
+          "rotation": 0,
+          "target": "0-render1"
+        },
+        {
+          "yaw": -2.1855670461777432,
+          "pitch": 0.28134508612242115,
+          "rotation": 0,
+          "target": "1-render2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "justatestPANO",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
